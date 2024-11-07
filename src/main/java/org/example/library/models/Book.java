1002ee39faa95d7733a8dcdc49b6879eab59022d
Daniil Sobolev <iwannabehappyrn@gmail.com>
2024-11-07 19:04:52 +0300

    1

---
+++ src/main/java/org/example/library/models/Book.java
@@ -0,0 +1,14 @@
+package org.example.library.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+}
